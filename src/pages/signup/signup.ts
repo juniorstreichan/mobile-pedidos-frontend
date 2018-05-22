c15636163cabf020c4b5acf2c334687ddf8a8cbb
Junior Streichan <juniorstreichan@gmail.com>
2018-05-22 13:05:37 -0400

    FormGroup de usuario

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,20 +1,54 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
- 
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @IonicPage()
 @Component({
-  selector: 'page-signup',
-  templateUrl: 'signup.html',
+  selector: "page-signup",
+  templateUrl: "signup.html"
 })
 export class SignupPage {
+  formGroup: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder
+  ) {
+    this.formGroup = formBuilder.group({
+      nome: [
+        "TESTE",
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(120)
+        ]
+      ],
+      email: ["teste@teste.com", [Validators.required, Validators.email]],
+      tipo: ["1", [Validators.required]],
+      cpfOuCnpj: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(11),
+          Validators.maxLength(14)
+        ]
+      ],
+      senha: ["", [Validators.required]],
+      complemento: ["", []],
+      logradouro: ["", []],
+      numero: ["", []],
+      bairro: ["", []],
+      cep: ["", [Validators.required]],
+      telefone1: ["", [Validators.required]],
+      telefone2: ["", []],
+      telefone3: ["", []],
+      estadoId: [null, [Validators.required]],
+      cidadeId: [null, [Validators.required]]
+    });
   }
-  signupUser(){
-    console.log('foi');
-  }
- 
 
+  signupUser() {
+    console.log("foi");
+  }
 }
