680e1249e93909576f5206381f426db0bc11dfa9
Junior Streichan <juniorstreichan@gmail.com>
2018-05-25 13:00:38 -0400

    Carregando imagens dos  produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,3 +1,4 @@
+import { API_CONFIG } from "./../../config/api.config";
 import { ProdutoService } from "./../../services/domain/produto.service";
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
@@ -19,13 +20,26 @@ export class ProdutosPage {
 
   ionViewDidLoad() {
     let categoria_id = this.navParams.get("categoria_id");
-    this.produtoService
-      .findByCategoria(categoria_id)
-      .subscribe(
+    this.produtoService.findByCategoria(categoria_id).subscribe(
+      response => {
+        this.items = response["content"];
+        this.loadImageUrls();
+      },
+      error => {}
+    );
+  }
+
+  loadImageUrls() {
+    for (var i = 0; i < this.items.length; i++) {
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id).subscribe(
         response => {
-          this.items = response["content"];
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${
+            item.id
+          }-small.jpg`;
         },
         error => {}
       );
+    }
   }
 }
--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,12 +1,20 @@
-import { API_CONFIG } from './../../config/api.config';
+import { API_CONFIG } from "./../../config/api.config";
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Rx";
 
 @Injectable()
 export class ProdutoService {
   constructor(public http: HttpClient) {}
 
   findByCategoria(categoria_id: string) {
-    return this.http.get(`${API_CONFIG.baseUrl}/produtos?categorias=${categoria_id}`);
+    return this.http.get(
+      `${API_CONFIG.baseUrl}/produtos?categorias=${categoria_id}`
+    );
+  }
+
+  getSmallImageFromBucket(id: string): Observable<any> {
+    let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`;
+    return this.http.get(url, { responseType: "blob" });
   }
 }
