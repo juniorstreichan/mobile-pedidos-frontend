dfc979a3f2981ab6c8f0b09d7830c4c533aa2cd0
juniorstreichan <juniorstreichan@gmail.com>
2018-06-07 12:43:22 -0400

    teste no android

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,6 +1,6 @@
 
 
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://mobile-pedidos.herokuapp.com",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/mobile-pedidos"
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
+import { HomePage } from './home';
+import { FormGroup, Validators } from "@angular/forms";
+import { FormBuilder } from "@angular/forms";
 import { Component } from "@angular/core";
-import { NavController, IonicPage, MenuController } from "ionic-angular";
+import { NavController, IonicPage, MenuController, AlertController } from "ionic-angular";
 import { AuthService } from "./../../services/auth.service";
 import { CredentialsDTO } from "./../../models/credentials.dto";
 
@@ -14,11 +17,20 @@ export class HomePage {
     senha: ""
   };
 
+  formGroup: FormGroup;
+
   constructor(
     public navCtrl: NavController,
     public menu: MenuController,
-    public auth: AuthService
-  ) {}
+    public auth: AuthService,
+    public formBuilder: FormBuilder,
+    public alertCtrl: AlertController
+  ) {
+    this.formGroup = formBuilder.group({
+      email: ["", [Validators.required]],
+      senha:["", [Validators.required]]
+    });
+  }
 
   ionViewWillEnter() {
     this.menu.swipeEnable(false);
@@ -44,12 +56,21 @@ export class HomePage {
         this.auth.successfullLogin(response.headers.get("Authorization"));
         this.navCtrl.setRoot("CategoriasPage");
       },
-      error => {}
+      error => {
+        if(error.status == 403){
+          let alert = this.alertCtrl.create({
+            title: 'Falha na autenticação',
+            subTitle: 'Login ou senha inválidos',
+            buttons: ['OK']
+          });
+          alert.present();
+          this.navCtrl.setRoot("HomePage");
+        }
+      }
     );
   }
 
-  signup(){
-    this.navCtrl.push('SignupPage');
-
+  signup() {
+    this.navCtrl.push("SignupPage");
   }
 }
